f319410c76ebbc4e3905b5d54ade441cad0a075d
neresm <neresm@innopharmalabs.com>
2017-09-13 15:25:00 +0100

    Added list of users

---
+++ src/public/app/controllers/home.controller.js
@@ -0,0 +1,15 @@
+"use strict";
+app.controller("HomeController", function(
+  $scope,
+  $state,
+  RESTService
+) {
+  RESTService.getUsers().then(
+    function(response) {
+      $scope.users = response.data.users;
+    },
+    function(err) {
+      console.log("Error to retrieve users", err);
+    }
+  );
+});
