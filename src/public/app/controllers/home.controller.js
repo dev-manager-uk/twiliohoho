61181e608ffbd0ebdac7de569bc1d42e0e19c560
Marcelo Neres <marcelo.neres1@gmail.com>
2017-09-13 23:30:39 +0100

    Added logic to user list colors

--- src/public/app/controllers/home.controller.js
+++ src/public/app/controllers/home.controller.js
@@ -2,6 +2,7 @@
 app.controller("HomeController", function(
   $scope,
   $state,
+  $interval,
   RESTService
 ) {
   RESTService.getUsers().then(
@@ -12,4 +13,26 @@ app.controller("HomeController", function(
       console.log("Error to retrieve users", err);
     }
   );
+
+  updateCalls();
+
+  $interval(function() {
+    updateCalls();
+  }, 2000);
+
+  function updateCalls(){
+    RESTService.getCallList().then(function(response) {
+      $scope.calls = response.data;
+      angular.forEach($scope.users, function(usersVal) {
+        usersVal.status = "free";
+      });
+      angular.forEach($scope.calls, function(callVal, callKey) {
+        angular.forEach($scope.users, function(usersVal) {
+          if (usersVal.number === callVal.to) {
+            usersVal.status = "busy";
+          }
+        });
+      });
+    });  
+  }
 });
