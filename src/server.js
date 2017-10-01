64674392ea40dd76d78db182050d5f453d3e03d5
Marcelo Neres <marcelo.neres1@gmail.com>
2017-10-01 16:18:10 +0100

    removed users.xml

--- src/server.js
+++ src/server.js
@@ -10,22 +10,6 @@ app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
-var XMLWriter = require('xml-writer'),
-               fs = require('fs');
-    var ws = fs.createWriteStream('./src/assets/users.xml');
-    ws.on('close', function() {
-            console.log(fs.readFileSync('./src/assets/users.xml', 'UTF-8'));
-    });
-    let xw = new XMLWriter(false, function(string, encoding) {
-            ws.write(string, encoding);
-    });
-    xw.startDocument('1.0', 'UTF-8').startElement(function() {
-        return 'root';
-    }).text(function() {
-        return 'Some content';
-    });
-    ws.end();
-
 //Create routes
 routes.generateRoutes(app);
 
