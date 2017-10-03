787d3778f828e96d05d5ef7423f5a19089952a9f
Marcelo Neres <marcelo.neres1@gmail.com>
2017-10-03 22:44:05 +0100

    added transfer function to users

--- src/public/app/controllers/home.controller.js
+++ src/public/app/controllers/home.controller.js
@@ -6,7 +6,6 @@ app.controller("HomeController", function(
   $stateParams,
   RESTService
 ) {
-
   $scope.selectedUser = null;
 
   RESTService.getUsers().then(
@@ -24,44 +23,88 @@ app.controller("HomeController", function(
     updateCalls();
   }, 2000);
 
-  function updateCalls(){
+  function updateCalls() {
     RESTService.getCallList().then(function(response) {
       $scope.calls = response.data;
-      angular.forEach($scope.users, function(usersVal) {
-        usersVal.status = "free";
-        if($stateParams.user === usersVal.text){
-          $scope.selectedUser = usersVal;
+      RESTService.getConferenceList().then(
+        function(response) {
+          $scope.conferences = response.data;
+          let selectedUserTmp = undefined;
+          angular.forEach($scope.users, function(usersVal) {
+            usersVal.status = "free";
+            if ($stateParams.user === usersVal.text) {
+              selectedUserTmp = usersVal;
+              if(selectedUserTmp.hasOwnProperty('conferenceSid')){
+                delete selectedUserTmp.conferenceSid;
+              }
+              if(selectedUserTmp.hasOwnProperty('friendlyName')){
+                delete selectedUserTmp.friendlyName;
+              }
+            }
+          });
+          angular.forEach($scope.calls, function(callVal, callKey) {
+            angular.forEach($scope.users, function(usersVal) {
+              if (usersVal.number === callVal.to) {
+                usersVal.status = "busy";
+              }
+              if ($stateParams.user === usersVal.text) {
+                selectedUserTmp = usersVal;
+              }
+            });
+          });
+          angular.forEach($scope.conferences, function(confVal, confKey) {
+            angular.forEach(confVal.participants, function(participant) {
+              if(selectedUserTmp.number === participant.to){
+                selectedUserTmp.conferenceSid = confVal.sid;
+                selectedUserTmp.friendlyName = confVal.friendlyName;
+              }
+            });
+          });
+          $scope.selectedUser = selectedUserTmp
+        },
+        function(err) {
+          console.log("err", err);
         }
-      });
-      angular.forEach($scope.calls, function(callVal, callKey) {
-        angular.forEach($scope.users, function(usersVal) {
-          if (usersVal.number === callVal.to) {
-            usersVal.status = "busy";
-          }
-          if($stateParams.user === usersVal.text){
-            $scope.selectedUser = usersVal;
-          }
-        });
-      });
-    });  
+      );
+    });
   }
 
-  $scope.call = function(userCalled, userCalling){
+  $scope.call = function(userCalled, userCalling) {
     console.log("calling...");
     let data = {
       called: userCalled,
       user: userCalling
-    }
+    };
     RESTService.callBetweenClients(data).then(
-      function(response){
+      function(response) {
         console.log("completed");
       },
-      function(err){
+      function(err) {
         console.log("err", err);
-      });
-  }
+      }
+    );
+  };
 
-  $scope.transfer = function(userNumber){
+  $scope.transfer = function(userNumber) {
     console.log("tranfering...", userNumber);
-  }
+  };
+
+  $scope.joinConference = function(UserNo) {
+    if(!$scope.selectedUser.hasOwnProperty('conferenceSid')){
+      console.log("User has no conference!!");
+    }
+    let data = {
+      conferenceSid: $scope.selectedUser.conferenceSid,
+      conferenceName: $scope.selectedUser.friendlyName,
+      callNo: UserNo
+    };
+    RESTService.createCallAndJoinConference(data).then(
+      function(response) {
+        console.log("response", response);
+      },
+      function(err) {
+        console.log("err", err);
+      }
+    );
+  };
 });
